4eb616075c0f918e4f9db17d1a9f31b43ff06855
Shresth Agrawal <shresthagrawal.31@gmail.com>
2021-09-09 12:09:27 +0200

    Export required functions
    Add prepare script in package.json

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,7 @@ export { SOR } from './wrapper';
 export { BPTForTokensZeroPriceImpact as weightedBPTForTokensZeroPriceImpact } from './frontendHelpers/weightedHelpers';
 export { BPTForTokensZeroPriceImpact as stableBPTForTokensZeroPriceImpact } from './frontendHelpers/stableHelpers';
 export { scale, bnum } from './utils/bignumber';
+export { getOnChainBalances } from './poolCaching/onchainData';
+export { MULTIADDR, VAULTADDR } from './constants';
+export { parseNewPool, getOutputAmountSwap } from './pools';
 export * from './types';
